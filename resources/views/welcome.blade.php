605bf888d0f6a0fe18fc90acbe5ba723135d502d
Konrad Socha <konrad.socha5@gmail.com>
2023-04-11 17:39:49 +0200

    Dodanie oddzielnych plików dla skryptów i css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        @include('general.layout.css')
     </head>
     <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
