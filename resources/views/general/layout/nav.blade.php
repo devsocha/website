b1c0517b00760add2852d55ff9d576a25d96b342
Konrad Socha <konrad.socha5@gmail.com>
2023-04-11 17:46:53 +0200

    Dodanie nav w oddzielnym pliku gotowym do dziedziczenia wraz z sekcjami

---
+++ resources/views/general/layout/nav.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title') - DevSocha.pl</title>
+    @include('general.layout.css')
+</head>
+<body>
+@include('general.layout.scripts')
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Strona główna</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Kursy</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">O mnie</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Kontakt</a>
+                </li>
+            </ul>
+            @if(!\Illuminate\Support\Facades\Auth::user())
+                <a class="d-flex btn btn-primary" role="search"> Zaloguj</a>
+                <a class="d-flex btn btn-primary" style="margin-left:1%" role="search"> Rejestracja</a>
+            @endif
+        </div>
+    </div>
+</nav>
+@yield('content')
+</body>
+</html>
