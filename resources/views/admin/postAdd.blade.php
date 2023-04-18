647dc053acf3a0dcc94075eb53176245bbf348e2
Konrad Socha <konrad.socha5@gmail.com>
2023-04-18 21:38:08 +0200

    Dodanie szablonu zakladki postpage w panelu admina oraz dodawania postu

---
+++ resources/views/admin/postAdd.blade.php
@@ -0,0 +1,20 @@
+@extends('admin.layout.nav')
+@section('title','Panel admina')
+@section('content')
+    <div class="row m-5">
+        <form>
+            <div class="mb-3">
+                <label for="title" class="form-label">Tytuł posta</label>
+                <input type="text" class="form-control" id="title" placeholder="Przykładowy tytuł - WOW">
+            </div>
+            <div class="mb-3">
+                <label for="desc" class="form-label">Opis</label>
+                <textarea class="form-control" id="desc" rows="10"placeholder="Przykładowy tekst - WOW"></textarea>
+            </div>
+            <div>
+                <input class="btn btn-primary "type="submit" value="Dodaj">
+                <a hef=""class="btn btn-secondary" >Cofnij</a>
+            </div>
+        </form>
+    </div>
+@endsection
--- resources/views/admin/postPage.blade.php
+++ resources/views/admin/postPage.blade.php
@@ -1,5 +1,30 @@
 @extends('admin.layout.nav')
 @section('title','Panel admina')
 @section('content')
-    elo
+    <div class="row m-5">
+        <a class="btn btn-primary" href="" >Dodaj nowy post</a>
+    </div>
+
+    <div class="row m-5">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th style="width:70%"scope="col">Tytuł</th>
+                <th scope="col">Akcje</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>
+                    <a class="btn btn-danger" href="">Usuń</a>
+                    <a class="btn btn-secondary" href="">Edytuj</a>
+                </td>
+            </tr>
+
+            </tbody>
+        </table>
+    </div>
 @endsection
